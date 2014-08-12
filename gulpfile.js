126bda0cedfeb59b4f55ad3c3be2e94d3b3d3aed
Chris Bateman <chrisb808@gmail.com>
2014-08-12 00:12:07 +0000

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
-//var sourcemaps = require('gulp-sourcemaps')
-//var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify')
 
 
 gulp.task('js', function () {
-    gulp.src(['src/**/app.js', 'src/**/*.js'])
-        //.pipe(sourcemaps.init())
+    gulp.src(['src/**/*.js'])
+        .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
         .pipe(ngAnnotate())
-        //.pipe(uglify())
-        //.pipe(sourcemaps.write())
-        //.pipe(sourcemaps.write('.'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('.'));
 });
 
