b90fa20c61b96f8fcd280f3a6cb0c2e29eb43a8e
Chris Bateman <chrisb808@gmail.com>
2014-09-23 00:31:31 -0500

    new songs, style updates

--- src/data.js
+++ src/data.js
@@ -1,59 +1,127 @@
 angular.module('AudioTrackr').factory('songFactory', function () {
 	return [
 		{
+			name: 'Don\'t Stop Me Now',
+			band: 'Queen',
+			link: 'https://itunes.apple.com/us/album/dont-stop-me-now/id27243728?i=27243788',
+			tracks: [
+				{
+					name: 'piano',
+					url: 'files/dontstopmenow/piano.mp3',
+				},
+				{
+					name: 'bass',
+					url: 'files/dontstopmenow/bass.mp3',
+				},
+				{
+					name: 'guitar',
+					url: 'files/dontstopmenow/guitar.mp3',
+				},
+				{
+					name: 'vocals',
+					url: 'files/dontstopmenow/voice.mp3',
+				},
+				{
+					name: 'backing vocals',
+					url: 'files/dontstopmenow/chorus.mp3',
+				},
+				{
+					name: 'drums',
+					url: 'files/dontstopmenow/drums.mp3',
+				}
+			]
+		},
+		{
+			name: 'Reckoner',
+			band: 'Radiohead',
+			link: 'https://itunes.apple.com/us/album/reckoner/id270079778?i=270079996',
+			tracks: [
+				{
+					name: 'guitar',
+					url: 'files/reckoner/guitar.mp3'
+				},
+				{
+					name: 'piano, strings',
+					url: 'files/reckoner/piano-strings.mp3'
+				},
+				{
+					name: 'bass',
+					url: 'files/reckoner/bass.mp3'
+				},
+				{
+					name: 'vocals',
+					url: 'files/reckoner/vocals.mp3'
+				},
+				{
+					name: 'backing vocals',
+					url: 'files/reckoner/backing vocals.mp3'
+				},
+				{
+					name: 'drums',
+					url: 'files/reckoner/drums.mp3'
+				}
+			]
+		},
+		{
+			name: 'White Knuckles',
+			band: 'OK Go',
+			link: 'https://itunes.apple.com/us/album/white-knuckles/id375464312?i=375464644',
+			tracks: [
+				{
+					name: 'guitar',
+					url: '//cbateman.com/demos/AudioTrackr/files/whiteknuckles/guitar.mp3'
+				},
+				{
+					name: 'bass',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/whiteknuckles/bass1.mp3'
+				},
+				{
+					name: 'synths',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/whiteknuckles/synths.mp3'
+				},
+				{
+					name: 'vocals',
+					url: '//cbateman.com/demos/AudioTrackr/files/whiteknuckles/vocals.mp3'
+				},
+				{
+					name: 'backing vocals',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/whiteknuckles/bg_vocals1.mp3'
+				},
+				{
+					name: 'drums',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/whiteknuckles/drums_combined.mp3'
+				},
+				{
+					name: 'kick ',
+					url: '//cbateman.com/demos/AudioTrackr/files/whiteknuckles/kick_1.mp3'
+				},
+				{
+					name: 'claps',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/whiteknuckles/claps.mp3'
+				}
+			]
+		},
+		{
 			name: 'Analog Or Digital',
 			band: 'Wildlife Control',
 			link: 'https://itunes.apple.com/us/album/analog-or-digital/id544588972?i=544588975',
 			tracks: [
 				{
-					name: 'vocals',
-					url: 'files/analogordigital/Analog or Digital.Vocals.mp3'
+					name: 'guitar, piano',
+					url: 'files/analogordigital/Analog or Digital.GuitarPiano.mp3'
 				},
 				{
-					name: 'guitarpiano',
-					url: 'files/analogordigital/Analog or Digital.GuitarPiano.mp3'
+					name: 'bass',
+					url: 'files/analogordigital/Analog or Digital.Bass.mp3'
+				},
+				{
+					name: 'vocals',
+					url: 'files/analogordigital/Analog or Digital.Vocals.mp3'
 				},
 				{
 					name: 'drums',
 					url: 'files/analogordigital/Analog or Digital.Drums.mp3'
 				},
-				{
-					name: 'bass',
-					url: 'files/analogordigital/Analog or Digital.Bass.mp3'
-				}
-			]
-		},
-		{
-			name: 'Lasso',
-			band: 'Phoenix',
-			link: 'https://itunes.apple.com/us/album/lasso/id315002203?i=315002473',
-			tracks: [
-				{
-					name: 'guitar',
-					url: 'files/lasso/Guitar.mp3'
-				},
-				{
-					name: 'bass',
-					url: 'files/lasso/Bass.mp3'
-				},
-				{
-					name: 'beat',
-					url: 'files/lasso/Beat.mp3'
-				},
-				{
-					name: 'key',
-					url: 'files/lasso/Key.mp3'
-				},
-				/*
-				{
-					name: 'roll',
-					url: 'files/lasso/Roll.mp3'
-				},
-				*/
-				{
-					name: 'vocals',
-					url: 'files/lasso/Accapela.mp3'
-				}
 			]
 		},
 		{
@@ -61,14 +129,6 @@ angular.module('AudioTrackr').factory('songFactory', function () {
 			band: 'Phoenix',
 			link: 'https://itunes.apple.com/us/album/armistice/id315002203?i=315002670',
 			tracks: [
-				{
-					name: 'beat',
-					url: 'files/armistice/Beat.mp3'
-				},
-				{
-					name: 'goblins',
-					url: 'files/armistice/Goblins.mp3'
-				},
 				{
 					name: 'guitar',
 					url: 'files/armistice/Guitar.mp3'
@@ -82,102 +142,110 @@ angular.module('AudioTrackr').factory('songFactory', function () {
 					url: 'files/armistice/Bass.mp3'
 				},
 				{
+					name: 'goblins',
+					url: 'files/armistice/Goblins.mp3'
+				},
+				{
 					name: 'vocals',
 					url: 'files/armistice/Accapela.mp3'
+				},
+				{
+					name: 'drums',
+					url: 'files/armistice/Beat.mp3'
 				}
 			]
 		},
 		{
-			name: "Lisztomania",
-			band: "Phoenix",
+			name: 'Lisztomania',
+			band: 'Phoenix',
 			link: 'https://itunes.apple.com/us/album/lisztomania/id315002203?i=315002364',
 			tracks: [
 				{
-					name: 'Bass',
+					name: 'guitar 1',
+					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar1.mp3'
+				},
+				{
+					name: 'guitar 2',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar2.mp3'
+				},
+				{
+					name: 'guitar 3',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar3.mp3'
+				},
+				{
+					name: 'bass',
 					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Bass-combined.mp3'
 				},
 				{
-					name: 'Beat',
-					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Beat.mp3'
+					name: 'cocotte',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Cocotte.mp3'
 				},
 				{
-					name: 'Brass',
-					url: '//s2.cbateman.com/demos/AudioTrackr/files/lisztomania/Brass.mp3'
-				},
-				{
-					name: 'Cocotte',
-					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Cocotte.mp3'
-				},
-				{
-					name: 'Guitar1',
-					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar1.mp3'
-				},
-				{
-					name: 'Guitar2',
-					url: '//s2.cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar2.mp3'
-				},
-				{
-					name: 'Guitar3',
-					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Guitar3.mp3'
-				},
-				{
-					name: 'Key',
-					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Key.mp3'
-				},
-				{
-					name: 'Motif',
+					name: 'motif',
 					url: '//s2.cbateman.com/demos/AudioTrackr/files/lisztomania/Motif.mp3'
 				},
 				{
-					name: 'Vocals',
-					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Vox.mp3'
+					name: 'brass',
+					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Brass.mp3'
+				},
+				{
+					name: 'keys',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/lisztomania/Key.mp3'
+				},
+				{
+					name: 'vocals',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/lisztomania/Vox.mp3'
+				},
+				{
+					name: 'drums',
+					url: '//cbateman.com/demos/AudioTrackr/files/lisztomania/Beat.mp3'
 				}
 			]
 		},
 		{
-			name: "Trying to be Cool",
-			band: "Phoenix",
+			name: 'Trying to be Cool',
+			band: 'Phoenix',
 			link: 'https://itunes.apple.com/us/album/trying-to-be-cool/id602308131?i=602308222',
 			tracks: [
 				{
-					name: 'Electric Guitars',
-					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/ALL ELEC GTRS.mp3'
-				},
-				{
-					name: 'Solinas',
-					url: '//s1.cbateman.com/demos/AudioTrackr/files/tryingtobecool/ALL SOLINAS.mp3'
-				},
-				{
-					name: 'Bass',
-					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/BASS.mp3'
-				},
-				{
-					name: 'Beat',
-					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/BEAT.mp3'
-				},
-				{
-					name: 'China Sonus Tone',
-					url: '//s1.cbateman.com/demos/AudioTrackr/files/tryingtobecool/CHINA SONUS TONE.mp3'
-				},
-				{
-					name: 'Glock Claps',
-					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/GLOCK CLAPS.mp3'
-				},
-				{
-					name: 'Guitar12',
+					name: 'guitar',
 					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/GTR12.mp3'
 				},
 				{
-					name: 'Moog',
+					name: 'electric guitars',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/tryingtobecool/ALL ELEC GTRS.mp3'
+				},
+				{
+					name: 'bass',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/BASS.mp3'
+				},
+				{
+					name: 'riff',
+					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/RIFF.mp3'
+				},
+				{
+					name: 'moog',
 					url: '//s1.cbateman.com/demos/AudioTrackr/files/tryingtobecool/MOOG MEGAWAVE.mp3'
 				},
 				{
-					name: 'Riff',
-					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/RIFF.mp3'
+					name: 'china sonus tone',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/CHINA SONUS TONE.mp3'
 				},
 				{
-					name: 'Vocals',
-					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/LEAD DRY.mp3'
+					name: 'solinas',
+					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/ALL SOLINAS.mp3'
+				},
+				{
+					name: 'vocals',
+					url: '//s1.cbateman.com/demos/AudioTrackr/files/tryingtobecool/LEAD DRY.mp3'
+				},
+				{
+					name: 'drums',
+					url: '//s2.cbateman.com/demos/AudioTrackr/files/tryingtobecool/BEAT.mp3'
+				},
+				{
+					name: 'glock claps',
+					url: '//cbateman.com/demos/AudioTrackr/files/tryingtobecool/GLOCK CLAPS.mp3'
 				}
 			]
 		}
